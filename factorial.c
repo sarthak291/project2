528416abcfdb1f3fd0f73c2a9fd54bcd2d1ab27c
sarthak291 <sarthakrevgade2005@gmail.com>
2025-09-24 19:39:10 +0530

    first commit

---
+++ factorial.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Function to calculate factorial recursively
+unsigned long long factorial(int n) {
+    if (n < 0) {
+        return 0; // Factorial not defined for negative numbers
+    }
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("Factorial of %d is %llu\n", num, factorial(num));
+    }
+    return 0;
+}
