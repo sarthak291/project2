d0efcc5a3c411941e52d0dfed4ed4c3cb524446b
sarthak291 <sarthakrevgade2005@gmail.com>
2025-09-29 19:50:05 +0530

    last ch

---
+++ javascriptcodepractice2.js
@@ -0,0 +1,17 @@
+//DOM: document object model
+//all html all component are node and nodes comes with element. and dom is all web page all over you sees is dom.
+
+//dom manupulation:
+//dom = body for now 
+//all website you see is dom
+//dom manupulation means change karna kya dom.
+//changing means after clicking download button start downloading message appears.
+
+//DOM manupulation ke liye kya kare
+//1) html se element select karna
+//2) text badalna
+//3) html badalna
+//4) css badalna
+//5) attributes badalna
+//6)event listners
+
